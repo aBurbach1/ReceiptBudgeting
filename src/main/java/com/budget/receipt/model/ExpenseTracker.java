2a5090241f93159f3de777a6d6ead687b411f0b3
aBurbach1 <burbach.aaron@gmail.com>
2021-07-06 22:09:30 -0500

    updated some models

--- src/main/java/com/budget/receipt/model/ExpenseTracker.java
+++ src/main/java/com/budget/receipt/model/ExpenseTracker.java
@@ -1,4 +1,14 @@
 package com.budget.receipt.model;
 
+import com.budget.receipt.model.expense.Expense;
+
+import java.util.List;
+
 public class ExpenseTracker {
+    private User user;
+    private List<Expense> expenses;
+
+    public void addExpense(Expense newExpense) {
+        this.expenses.add(newExpense);
+    }
 }
--- src/main/java/com/budget/receipt/model/expense/Expense.java
+++ src/main/java/com/budget/receipt/model/expense/Expense.java
@@ -1,4 +1,29 @@
 package com.budget.receipt.model.expense;
 
 public class Expense {
+    private Cost cost;
+    private Category category;
+
+    public Cost getCost() {
+        return cost;
+    }
+
+    public void setCost(Cost cost) {
+        this.cost = cost;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean equals(Expense otherExpense) {
+        if(this.category.toString().equals(otherExpense.toString()) && this.cost.equals(otherExpense.cost)) {
+            return true;
+        }
+        else return false;
+    }
 }
